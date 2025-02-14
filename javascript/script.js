4ccdb6e05553c0a9d9b2cbb3d553e4fa61b9d04e
rma4921 <rma4921@naver.com>
2025-02-14 16:27:36 +0900

    modify js file | add input function

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ const imgList = document.querySelectorAll(".imgBox img");
 const closeBtn = document.getElementById("closeBtn");
 const popupImg = document.getElementById("popupImg");
 const popup = document.getElementById("popup");
+const searchInput = document.getElementById("searchInput");
 
 imgList.forEach(img => {
     img.addEventListener("click", function(){
@@ -13,11 +14,20 @@ imgList.forEach(img => {
 closeBtn.addEventListener("click", function(){
     popup.style.display = "none";
 });
-/*popup.addEventListener("click", function(){
-    popup.style.display = "none";
-});*/
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'Escape' && popup.style.display === 'flex') {
-        popup.style.display = 'none';
+popup.addEventListener("click", function(background){
+    if (background.target === popup) {
+        popup.style.display = "none";
     }
+});
+document.addEventListener("keydown", function(esc) {
+    if (esc.key === "Escape" && popup.style.display === "flex") {
+        popup.style.display = "none";
+    }
+});
+searchInput.addEventListener("input", function(){
+    const filter = this.value.toLowerCase();
+    imgList.forEach(img => {
+        let altText = img.alt;
+        img.style.display = altText.includes(filter) ? "inline" : "none";
+    });
 });
