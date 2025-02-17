b6ca67776f361e790680f72462540a2c90506c78
rma4921 <rma4921@naver.com>
2025-02-17 16:57:43 +0900

    modify js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,44 @@
 const imgList = document.querySelectorAll(".imgBox img");
+const modal = document.getElementById("parentModal");
+const modalTitle = document.getElementById("modalTitle");
 const closeBtn = document.getElementById("closeBtn");
-const popupImg = document.getElementById("popupImg");
-const popup = document.getElementById("popup");
+const modalImg = document.getElementById("modalImg");
 const searchInput = document.getElementById("searchInput");
+
+function openModel(img){
+document.get
+    modal.classList.add("show");
+    document.body.style.overflow = 'hidden';
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+}
+function closeModel(){
+    modal.classList.remove("show");
+    document.body.style.overflow = 'auto';
+}
 
 imgList.forEach(img => {
     img.addEventListener("click", function(){
-        popup.style.display = "flex";
-        popupImg.src = img.src;
-        popupImg.alt = img.alt;
+        openModel(img);
     });
 });
 closeBtn.addEventListener("click", function(){
-    popup.style.display = "none";
+    closeModel();
 });
-popup.addEventListener("click", function(background){
-    if (background.target === popup) {
-        popup.style.display = "none";
+modal.addEventListener("click", function(background){
+    if (background.target === modal) {
+        closeModel();
     }
 });
 document.addEventListener("keydown", function(esc) {
-    if (esc.key === "Escape" && popup.style.display === "flex") {
-        popup.style.display = "none";
+    if (esc.key === "Escape" && modal.classList.contains("show")) {
+        closeModel();
     }
 });
 searchInput.addEventListener("input", function(){
     const filter = this.value.toLowerCase();
     imgList.forEach(img => {
-        let altText = img.alt;
+        let altText = img.alt.toLowerCase();
         img.style.display = altText.includes(filter) ? "inline" : "none";
     });
 });
